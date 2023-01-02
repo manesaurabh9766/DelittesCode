dc521650d15eb9b0267009ed2378f140d3b4900c
manesaurabh9766 <121089250+manesaurabh9766@users.noreply.github.com>
2023-01-02 14:48:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 8081
+
+app.get('/', (req, res) => {
+  res.sendFile("/public/index.html",{root:__dirname});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
